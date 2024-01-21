15bedf409d1afd5bc5f7577fc947050fd67fd8dc
ArizCefDev <arizcefdev@gmail.com>
2024-01-21 18:22:30 +0400

    Add project files.

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=WIN-VD08C7OPT8H\\SQLEXPRESS; database=ValyutaAPIDB; integrated security=true;"
+                );
+        }
+    }
+}
